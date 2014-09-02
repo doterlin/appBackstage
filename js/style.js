a347c9a8ebcf51e9dc6f6e43f7e3278cbb3b6db7
doterlin <453845311@qq.com>
2014-09-02 16:10:02 +0800

    Signed-off-by: doterlin <453845311@qq.com>

--- js/style.js
+++ js/style.js
@@ -8,13 +8,21 @@ $(document).ready(function(){
 	}).mouseout(function(){
 		$(this).removeClass("btn_hover").addClass("btn");
 	});
+	/*状态颜色*/
+
 	/*新增*/
 	$("#add").click(function(){
 		$(".alert_bg").css("display","block");
 		$(".alert").fadeIn("fast");
 	});
+	$("td a").click(function(){
+		if($(this).text()=="修改"){
+			$(".alert_bg").css("display","block");
+			$(".alert").fadeIn("fast");}
+	});
 	$("#cancel").click(function(){	
 		$(".alert").fadeOut("fast");
 		$(".alert_bg").css("display","none");
 	});
+	/*返回*/
 });
