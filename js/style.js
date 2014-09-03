2741cb707e4b70bfc9788a87d45220044553be99
doterlin <453845311@qq.com>
2014-09-03 17:57:46 +0800

    Signed-off-by: doterlin <453845311@qq.com>

--- js/style.js
+++ js/style.js
@@ -40,5 +40,13 @@ $(document).ready(function(){
 		$(".alert").fadeOut("fast");
 		$(".alert_bg").css("display","none");
 	});
-
+	/* 获取图片路径并显示，可选删除*/
+	$("#img_upload").change(function(){
+		$("#img_display img").attr("src","images/u244.png");//修改此路径显示图片
+		$("#img_display").append('<span class="text_gray tar wp100">[ <a id="img_display_delete" href="#" class="text_gray">删除</a> ]</span>');
+	});
+	$("#img_display a").click(function(){
+		alert("111");
+		$("#img_display img").remove();
+	})；
 });
